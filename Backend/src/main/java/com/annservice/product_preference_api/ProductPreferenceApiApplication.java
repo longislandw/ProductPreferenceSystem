0a68bd80db97c7576e1c3cf0e6fd902dfc06abfb
伯茶紅爵 <wjajohn01@gmail.com>
2025-07-18 03:50:15 +0800

    初始化backend, 建立spring boot專案 h Please enter the commit message for your changes. Lines starting

---
+++ Backend/src/main/java/com/annservice/product_preference_api/ProductPreferenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.annservice.product_preference_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPreferenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPreferenceApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/annservice/product_preference_api/ProductPreferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annservice.product_preference_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPreferenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
