c66b91d85266587480dbc491c49c110063ade2e9
Robert Wu <wjajohn01@gmail.com>
2025-07-19 02:09:22 +0800

    likelist table設定product對user的UniqueConstraint, 以避免同樣的商品被同一位使用者重複加入「喜好清單」

--- DB/schema.sql
+++ DB/schema.sql
@@ -24,5 +24,6 @@ CREATE TABLE like_list (
     user_id VARCHAR(50) NOT NULL,
     product_no BIGINT NOT NULL,
     CONSTRAINT fk_like_user FOREIGN KEY (user_id) REFERENCES user_table(user_id),
-    CONSTRAINT fk_like_product FOREIGN KEY (product_no) REFERENCES product(product_no)
+    CONSTRAINT fk_like_product FOREIGN KEY (product_no) REFERENCES product(product_no),
+    CONSTRAINT uc_user_product UNIQUE (user_id, product_no)  -- 設定一個使用者與一項商品只能對應一筆資料
 );
