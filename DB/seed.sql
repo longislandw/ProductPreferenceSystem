046ff2a114e52fb1128de6bb60146253fe0b39a0
Robert Wu <wjajohn01@gmail.com>
2025-07-30 16:54:50 +0800

    測試資料在密碼內加入加密方式的提示

--- DB/seed.sql
+++ DB/seed.sql
@@ -1,9 +1,9 @@
 -- User 資料
 INSERT INTO user_table (user_id, user_name, email, account, ppassword)
-VALUES ('A1236456789', '王O明', 'test@email.com', '1111999666', '$2a$10$P/I2HrXQobQdeyowWfG6Leslq.IvVBruVClh/MyjrEZpaMXBvdBy2'),
-       ('0000', '訪客', 'visitor@email.com', '00000000', '$2a$10$QGSWI8lbtzduxN8fwer/8.VE3ja0anxRIdPdhTDPqroDU4xzhm5HK'),
-       ('A0000000001', 'Ariel', 'ariel@email.com', '2222999667', '$2a$10$QGSWI8lbtzduxN8fbYt/8.VE3ja0anxRIdPdhTDPqroDU4xzhm5HK'),
-       ('0001', 'ADMIN', 'admin@email.com', '00000001', '$2a$10$P/I2HrXQobQdeyowWfG6Leslq.IvVBruVClh/MyjrEZpaMXBvdBy2');
+VALUES ('A1236456789', '王O明', 'test@email.com', '1111999666', '{bcrypt}$2a$10$P/I2HrXQobQdeyowWfG6Leslq.IvVBruVClh/MyjrEZpaMXBvdBy2'),
+       ('0000', '訪客', 'visitor@email.com', '00000000', '{bcrypt}$2a$10$QGSWI8lbtzduxN8fwer/8.VE3ja0anxRIdPdhTDPqroDU4xzhm5HK'),
+       ('A0000000001', 'Ariel', 'ariel@email.com', '2222999667', '{bcrypt}$2a$10$QGSWI8lbtzduxN8fbYt/8.VE3ja0anxRIdPdhTDPqroDU4xzhm5HK'),
+       ('0001', 'ADMIN', 'admin@email.com', '00000001', '{bcrypt}$2a$10$P/I2HrXQobQdeyowWfG6Leslq.IvVBruVClh/MyjrEZpaMXBvdBy2');
 
 -- User Authority 資料
 INSERT INTO user_authority (authority, description) VALUES ('VISITOR', '訪客權限'), ('USER', '一般使用者'), ('ADMIN', '管理員');
